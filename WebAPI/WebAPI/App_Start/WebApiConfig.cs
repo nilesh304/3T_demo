68b7f1a60e2ddaab548a351395e9cc46aaa8a8bd
Nilesh Matai <matainilesh678@gmail.com>
2019-06-07 02:47:05 +0530

    mplementing 3 tier arch using Angular7 .NET and SQL Server

---
+++ WebAPI/WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Web.Http.Cors;
+
+namespace WebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            config.EnableCors();
+            config.MapHttpAttributeRoutes();
+            //Enable Cors
+            //config.EnableCors(new EnableCorsAttribute("http://localhost:4200/", headers: "*", methods: "*"));
+
+            
+            //config.EnableCors(new EnableCorsAttribute("http://localhost:4200/",headers:"*",methods:"*"));
+
+            // Web API configuration and services
+
+            // Web API routes
+            //config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
